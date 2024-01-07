abba1fb7502ac6dbe2f3758817d8b5a5b898a40c
sreekrishnan-r <57753860+sreekrishnan-r@users.noreply.github.com>
2024-01-07 12:25:01 +0530

    Update app.py
    updated app.py

--- app.py
+++ app.py
@@ -3,9 +3,22 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World !!!"
+app.config['MYSQL_HOST'] = 'sreekrishnan-flask-mysql-server.mysql.database.azure.com'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'admin'
+app.config['MYSQL_PASSWORD'] = 'admin'
+app.config['MYSQL_DB'] = 'sreekrishnan-flask-mysql-database'
+
+@app.route('/')
+def example():
+   with app.app_context():
+      cursor = mysql.connection.cursor()
+      cursor.execute('show databases')
+      data = cursor.fetchall()
+      cursor.close()
+   print(len(data))
+   
+   return render_template('index.html', data_len = len(data), data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
